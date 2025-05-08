7bcdeb3daf613193847467e736915ff793b6e53d
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2025-05-08 11:17:09 +0300

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,22 @@
+use reqwest::Client;
+
+#[tokio::main]
+async fn main(){
+  let client:Client = Client::new();
+
+  let response:RequestBuilder = client.get("https://scrapeme.live/shop/").send().await.unwrap();
+  let html_content:String = response.text().await.unwrap();
+  let document:Html = scraper::Html::parse_document(&html_content);
+  let html_product_selector:Result<Selector,SelectorErrorKind> = scraper::Selector::parse("li.product").unwrap();
+
+  let html_products:Select = document.select(&html_product_selector);
+
+  for product in html_products{
+    let url = product.select(&scraper::Selector::parse("a").unwrap()).next().and_then(|a|a.value().attr("href")).map(str::to_owned);
+    let image_url = product.select(&scraper::Selector::parse("img").unwrap()).next().and_then(|a|a.value().attr("src")).map(str::to_owned);
+    let product_name = product.select(&scraper::Selector::parse("h2").unwrap()).next().map(|h2|h2.text().collect::<String>());
+    let product_price = product.select(&scraper::Selector::parse(".price").unwrap()).next().map(|h2|h2.text().collect::<String>());
+
+    println!("name = {:?}, price = {:?}, url = {:?}, image_url = {:?}", product_name, product_price, url, image_url);
+  }
+}
